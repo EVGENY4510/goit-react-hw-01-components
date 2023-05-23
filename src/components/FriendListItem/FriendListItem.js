271c968678b1abb179a0ccb23a439ec1671430b3
EVGENY4510 <lentiay4510z@gmail.com>
2023-05-23 15:31:17 +0300

    chenge css condition

--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -4,12 +4,14 @@ import css from 'components/FriendListItem/FriendListItem.module.css';
 export default function FriendListItem({ avatar, name, isOnline }) {
   return (
     <>
-      {isOnline ? (
+      {/* {isOnline ? (
         <span className={css.statusOnline}></span>
       ) : (
         <span className={css.statusOffline}></span>
-      )}
-      {/* <span className="status"></span> */}
+      )} */}
+      <span
+        className={`${isOnline ? css.statusOnline : css.statusOffline}`}
+      ></span>
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
     </>
